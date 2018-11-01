0a151040fbc4d7c30d6593514d6e0dd6a27f62ac
Laura Watson <laura.watson@castlightfinancial.com>
2018-11-01 16:39:21 +0000

    update tests

--- test/controllers/user_images_controller_test.rb
+++ test/controllers/user_images_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class UserImagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @user_image = user_images(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
